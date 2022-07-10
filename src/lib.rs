c728956c3d2d283d3b4b99a1e6d8fdaa8debdc55
Pietro Tamilia <kapakapa197@gmail.com>
2022-07-10 23:10:00 +0200

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use std::marker::PhantomData;
+
+pub trait Iterify: Sized {
+    fn iterify<I, F: FnMut(&mut Self) -> Option<I>>(&mut self, op: F) -> Iterated<Self, I, F> {
+        Iterated(self, op, PhantomData)
+    }
+}
+
+pub struct Iterated<'a, K, I, F: FnMut(&mut K) -> Option<I>>(&'a mut K, F, PhantomData<K>);
+impl<'a, K, I, F: FnMut(&mut K) -> Option<I>> Iterator for Iterated<'a, K, I, F> {
+    type Item = I;
+    fn next(&mut self) -> Option<Self::Item> {
+        (self.1)(self.0)
+    }
+}
+
+impl<T> Iterify for T {}
